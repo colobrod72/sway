b169cc66200b1a1b9c763785fb659d915c34f79a
colobrod72 <ftx1192@gmail.com>
2026-03-12 12:15:17 +0000

    Delete mango.sh

--- mango.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y rofi foot xdg-desktop-portal-wlr swaybg waybar wl-clipboard wlsunset wlogout dunst pamixer wlr-dpms sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout sox
-git clone https://github.com/DreamMaoMao/mango-config.git ~/.config/mango
