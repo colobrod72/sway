35a342aab5a38da2ac83eb1aa3376c14967fcfa3
colobrod72 <ftx1192@gmail.com>
2026-03-12 12:35:26 +0000

    Delete labs.sh

--- labs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt install libcairo2-dev libpango1.0-dev libxml2-dev libglib2.0-dev meson gcc wlroots wayland-protocols cairo pango libxml2 glib2
-git clone https://github.com/labwc/labwc
-cd labwc/
-meson setup build/
-meson compile -C build/
