31d449d8334793c80e95ac18650c9af44cffd744
colobrod72 <ftx1192@gmail.com>
2026-03-22 11:26:02 +0100

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -2,6 +2,6 @@
 sudo pacman -Syu
 sudo pacman -Sc
 mv profile /home/zx/.bash_profile && mv gtklock /home/zx/.config/sway && mv oboi /home/zx/.config/sway && mv rofi /home/zx/.config/sway && mv scripts /home/zx/.config/sway && mv swaync /home/zx/.config/sway && mv waybar /home/zx/.config/sway && mv wofi /home/zx/.config/sway && mv config /home/zx/.config/sway
-sudo pacman -S sway swayidle swaylock swaybg waybar wofi wmenu terminology geany autotiling sway-notification-center grim slurp wl-clipboard cliphist brightnessctl playerctl wlr-randr wdisplays kanshi xdg-desktop-portal-wlr swappy wtype thunar thunar-archive-plugin pavucontrol pamixer network-manager-applet nwg-look eog ttf-jetbrains-mono-nerd gawk polkit bleachbit rofi
+sudo pacman -S sway swayidle swaylock swaybg waybar wofi wmenu terminology geany autotiling grim slurp wl-clipboard cliphist brightnessctl playerctl wlr-randr wdisplays kanshi xdg-desktop-portal-wlr swappy wtype thunar thunar-archive-plugin pavucontrol pamixer network-manager-applet nwg-look eog ttf-jetbrains-mono-nerd gawk polkit bleachbit rofi
 
 
